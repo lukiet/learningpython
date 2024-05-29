b595e3760118d5b6694bba42783fab25bcec754f
Luke Tsalwa <luketsalwa@gmail.com>
2024-05-29 10:44:35 +0300

    cs

---
+++ conditionalStt.py
@@ -0,0 +1,11 @@
+age = int(input('Enter your age: '))
+if age >= 18:
+    print('You are an adult')
+elif age >= 13:
+    print('You are a teenager')
+else:
+    print('You are a child')
+
+print('You are', age, 'years old')
+
+# The code above is a conditional statement that checks the age of the user and prints a message based on the age.
