7043d85b5d4f52384b04f79b5230f896cb5e5446
Luke Tsalwa <luketsalwa@gmail.com>
2024-05-29 11:12:47 +0300

    for else loops

---
+++ for_else.py
@@ -0,0 +1,7 @@
+names = ["Luke, Tsalwa"]
+for name in names:
+    if name.startswith("L"):
+        print("Found")
+        break
+else:
+    print("Not Found")
--- loops_for.py
+++ loops_for.py
@@ -4,8 +4,7 @@ for x in "python":
 for x in ['a', 'b', 'c']:
     print(x)
 
-for x in range(0, 10):
-    print(x)
+# for x in range(0, 10) print(x)
 
 print(range(5))
 print([1, 2, 3, 4, 5])
