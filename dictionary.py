70dc9fc4f5438ba7a6bb70a4888f679ca301558c
Luke Tsalwa <luketsalwa@gmail.com>
2024-07-22 08:31:50 +0300

    n

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+details = {
+    'name': 'Luke',
+    'age': 27,
+    'Location': 'Kenya'
+}
+print(details)
+print(details['name'])
+print(details.get('age'))
+
+# update the dictionary
+details['Location'] = 'Uganda'
