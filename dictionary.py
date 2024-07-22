4a6ba230d51017374614313c90fc31afcdcbfa96
Luke Tsalwa <luketsalwa@gmail.com>
2024-07-22 08:50:15 +0300

    n

--- dictionary.py
+++ dictionary.py
@@ -28,3 +28,14 @@ print(details)
 # delete a key-value pair
 del details['hometown']
 print(details)
+
+# create a dictionary using the comprehension method
+new_details = {x: x**2 for x in range(10)}
+print(new_details)
+
+# iterate through the dictionary
+details2 = {1: 'Luke', 2: 'John', 3: 'Mary'}
+for i in details2:
+    print(details2[i])
+
+print(len(details2))
