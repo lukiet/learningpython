d1d788ca900668f79277ae43a2aa6a9f8b1259ee
Luke Tsalwa <luketsalwa@gmail.com>
2024-07-22 08:40:06 +0300

    n

--- dictionary.py
+++ dictionary.py
@@ -8,9 +8,23 @@ print(details['name'])
 print(details.get('age'))
 
 # update the dictionary
-details['Location'] = 'Uganda'
+details['Location'] = 'Britain'
 print(details)
 
 # add a new key-value pair
 details['race'] = 'African'
+details['hometown'] = 'Kitale'
+details['tribe'] = 'Luhya'
+print(details)
+
+# remove a particular key-value pair
+details.pop('race')
+print(details)
+
+# remove the last key-value pair
+details.popitem()
+print(details)
+
+# delete a key-value pair
+del details['hometown']
 print(details)
