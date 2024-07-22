17f9109d38b567a02bec249ead6459c8e6972cab
Luke Tsalwa <luketsalwa@gmail.com>
2024-07-22 08:11:10 +0300

    new

---
+++ directory_filamanagement.py
@@ -0,0 +1,10 @@
+import os
+print(os.getcwd())  # get current working directory
+print(os.getcwdb())  # get current working directory as bytes object
+# change current working directory
+os.chdir('C:\\Users\\luket\\OneDrive\\Desktop\\projects\\school\\python\\learningpython')
+# list all files and directories in the current working directory
+print(os.listdir())
+# os.mkdir('newdir')  # create a new directory
+os.rename('newdir', 'newdir2')  # rename a directory
+print(os.listdir())
