353af429f62ab5b5c9515dabad13c65317e9a38c
lukiet <luketsalwa@gmail.com>
2025-07-22 11:35:39 +0300

    calculator

--- logictest.py
+++ logictest.py
@@ -1,8 +1,11 @@
-name = ""
+name = input("What is your name? ")
 
 if not name.strip():
     print("Name is empty")
 
-age = 27
+
+
+age = int(input("How old are you?"))
+
 if 18 <= age < 45:
     print('You are not old')
