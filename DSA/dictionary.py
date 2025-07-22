9220d78618cc7570609cac336ad0cffe0855e471
lukiet <luketsalwa@gmail.com>
2025-07-22 12:09:27 +0300

    dictionary

--- DSA/dictionary.py
+++ DSA/dictionary.py
@@ -1,5 +1,13 @@
 weekConversion = {
     "Mon" : "Monday",
     "Tue" : "Tuesday",
-
+    "Wed" : "Wednesday",
+    "Thur" : "Thursday",
+    "Fri" : "Friday",
+    "Sat" : "Thursday",
+    "Sun" : "Sunday",
 }
+
+print(weekConversion['Mon'])
+
+print(weekConversion.get("Tue"))
