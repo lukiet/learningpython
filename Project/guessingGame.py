4cbd2048b7ee022c995bc132088b3c15ef19e70a
lukiet <luketsalwa@gmail.com>
2025-07-22 12:38:02 +0300

    project

--- Project/guessingGame.py
+++ Project/guessingGame.py
@@ -1,4 +1,13 @@
-guess = int(input('Enter a number between 1 - 100 : '))
+from random import randint
 
-num = rand()%100+1
+guess = int(input('Enter a number between 1 - 10 : '))
 
+num = randint(1, 10)
+
+while guess !=num:
+    if guess == num:
+        print('Congrats you won !')
+    elif guess > num:
+        print('Try a smaller number ')
+    elif guess < num:
+        print('Try a bigger number ')
